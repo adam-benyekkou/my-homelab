d49a6a90707f6bded264301ed6abefbb94bb242d
CavyDev <cavy.protocol.dev@proton.me>
2026-01-22 21:27:25 +0000

    Added terraform config for the server based one my Hetzner VPS

---
+++ terraform/main.tf
@@ -0,0 +1,18 @@
+resource "hcloud_ssh_key" "cavy_key" {
+  name       = "cavy-ssh-key"
+  public_key = file("~/.ssh/id_ed25519.pub")
+}
+
+# The Server definition
+resource "hcloud_server" "cavy_vps" {
+  name        = "cavy-vps"
+  image       = "debian-12"
+  server_type = var.server_type
+  location    = var.location
+  ssh_keys    = [hcloud_ssh_key.cavy_key.id]
+
+  public_net {
+    ipv4_enabled = true
+    ipv6_enabled = true
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,12 @@
+variable "hcloud_token" {
+  type      = string
+  sensitive = true
+}
+
+variable "location" {
+  default = "nbg1"
+}
+
+variable "server_type" {
+  default = "cx32" 
+}
